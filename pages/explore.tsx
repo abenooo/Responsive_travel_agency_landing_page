5afae1053f1e9fe1194a918daa5e98e1c7e43269
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2022-12-31 14:11:47 +0300

    Create explore page

---
+++ pages/explore.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+function explore() {
+    return (
+        <>
+            <Navbar />
+            <div>explore</div>
+            <Footer />
+        </>
+    )
+}
+
+export default explore
