f3d4f8eb662d97c4941fae65c3ca961d097911dd
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2022-12-31 14:46:56 +0300

    Adding dicover page

---
+++ pages/discover.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+function discover() {
+    return (
+        <>
+            <Navbar />
+            <div>discover
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default discover
