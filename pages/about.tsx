3d751728079158867fcdd42335c58dbeb9fad4aa
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2022-12-31 14:10:08 +0300

    Create about page

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+function about() {
+  return (
+    <>
+    <Navbar />
+    <div>about</div>
+    <Footer />
+    </>
+  )
+}
+
+export default about
